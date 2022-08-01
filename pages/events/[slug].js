4224d8c4caea6ebc157563ad0674437ee4ca6233
fry <fry.land@hotmail.fr>
2022-08-01 07:30:13 +0200

    staticPaths & Props for  event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,45 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   console.log(slug);
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: { evt: events[0] },
+//   };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true, // If false will return 404 if path is not found
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log(slug);
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { evt: events[0] },
+    revalidate: 1,
+  };
 }
